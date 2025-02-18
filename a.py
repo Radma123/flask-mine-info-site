12258841433364b4a388898504318fe64a2bcab5
Radmir <iradmir1516@gmail.com>
2025-02-18 17:33:16 +0500

    -

--- a.py
+++ a.py
@@ -1,23 +1,25 @@
 from g4f.client import Client
 
 client = Client()
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "Что изображено на этом фото?"},
-                {"type": "image_url", "image_url": {"url": "https://cdn1.insta360.ru/wp-content/uploads/2018/04/sandisk-extreme-pro-uhs-i-sd-128gb-500x500-min.jpg"}}
-            ]
-        }
-    ],
-    web_search = False
+# response = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": [
+#                 {"type": "text", "text": "Что изображено на этом фото?"},
+#                 {"type": "image_url", "image_url": {"url": "https://cdn1.insta360.ru/wp-content/uploads/2018/04/sandisk-extreme-pro-uhs-i-sd-128gb-500x500-min.jpg"}}
+#             ]
+#         }
+#     ],
+#     web_search = False
+# )
+
+# print(response.choices[0].message.content)
+
+response = client.images.generate(
+    model="flux",
+    prompt="a white siamese cat",
+    response_format="b64_json"
 )
-
-print(response.choices[0].message.content)
-
-
-
-# import os
-# os.mkdir("static/images")
+print(response.data[0])
