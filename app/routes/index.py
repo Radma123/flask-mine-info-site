fea1cc816e476eea08f661267cf9f6e70171bd74
Radmir <iradmir1516@gmail.com>
2025-03-09 15:11:56 +0500

    robots.txt

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, redirect, render_template, request
+from flask import Blueprint, abort, current_app, redirect, render_template, request, send_from_directory
 from flask_login import current_user, login_required
 
 index = Blueprint('index', __name__)
@@ -6,3 +6,7 @@ index = Blueprint('index', __name__)
 @index.route('/', methods = ['POST', 'GET'])
 def index_page():
     return render_template('main/index.html')
+
+@index.route('/robots.txt')
+def robots():
+    return send_from_directory(current_app.config['ROOT'], 'robots.txt')
